9360cec183c485749efdffcafa8198d639e85e5b
Siddhant Shukla <siddhantshukla356@gmail.com>
2019-04-29 22:03:40 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -2,4 +2,4 @@ var playlist={Sia:'Audio',Eminiem:'Rap God',CharliPuth:'See you again'};
 function updatePlaylist(playlist, artistName, songTitle){
   return Object.assign({}, playlist, {[artistName]: songTitle})
 }
-function update
+function removeFromPlaylist()
